29b6ca6d04287528938fae43db327355452d4c3f
Shrihan Samayamanthula <shrihan127@gmail.com>
2026-04-07 18:42:26 -0500

    Configure Firebase client settings for GenZearn project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Replace these placeholder values with your Firebase project configuration.
 window.firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCna2awece-Xe7d_l0IeJ97cGXrnxZE0es",
+  authDomain: "genzearn-a0313.firebaseapp.com",
+  databaseURL: "https://genzearn-a0313-default-rtdb.firebaseio.com",
+  projectId: "genzearn-a0313",
+  storageBucket: "genzearn-a0313.firebasestorage.app",
+  messagingSenderId: "514449582943",
+  appId: "1:514449582943:web:58dea5dcf7d88263db64ea",
+  measurementId: "G-TD6NPH9PVE"
 };
