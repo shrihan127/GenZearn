c35af42af2238c8c8e9579421f6979a500a7901a
Shrihan Samayamanthula <shrihan127@gmail.com>
2026-04-21 19:47:29 -0500

    Initialize Firebase from shared config file

--- firebase-config.js
+++ firebase-config.js
@@ -8,3 +8,13 @@ window.firebaseConfig = {
   appId: "1:514449582943:web:58dea5dcf7d88263db64ea",
   measurementId: "G-TD6NPH9PVE"
 };
+
+var firebaseConfig = window.firebaseConfig;
+
+if (
+  window.firebase &&
+  typeof window.firebase.initializeApp === 'function' &&
+  (!window.firebase.apps || window.firebase.apps.length === 0)
+) {
+  firebase.initializeApp(firebaseConfig);
+}
