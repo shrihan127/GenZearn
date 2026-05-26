dd998a7a07b126748e3c51792c3e28a53c3849eb
Shrihan Samayamanthula <shrihan127@gmail.com>
2026-05-25 22:28:27 -0500

    Refactor Firebase bootstrap to single modular source

--- firebase.js
+++ firebase.js
@@ -1,30 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCna2awece-Xe7d_l0IeJ97cGXrnxZE0es",
-  authDomain: "genzearn-a0313.firebaseapp.com",
-  databaseURL: "https://genzearn-a0313-default-rtdb.firebaseio.com",
-  projectId: "genzearn-a0313",
-  storageBucket: "genzearn-a0313.firebasestorage.app",
-  messagingSenderId: "514449582943",
-  appId: "1:514449582943:web:58dea5dcf7d88263db64ea",
-  measurementId: "G-TD6NPH9PVE"
+  apiKey: 'AIzaSyCna2awece-Xe7d_l0IeJ97cGXrnxZE0es',
+  authDomain: 'genzearn-a0313.firebaseapp.com',
+  databaseURL: 'https://genzearn-a0313-default-rtdb.firebaseio.com',
+  projectId: 'genzearn-a0313',
+  storageBucket: 'genzearn-a0313.firebasestorage.app',
+  messagingSenderId: '514449582943',
+  appId: '1:514449582943:web:58dea5dcf7d88263db64ea',
+  measurementId: 'G-TD6NPH9PVE'
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 const rtdb = getDatabase(app);
 
-let analytics = null;
-if (typeof window !== "undefined") {
-  isSupported().then((supported) => {
-    if (supported) {
-      analytics = getAnalytics(app);
-    }
-  });
-}
-
-export { app, db, rtdb, analytics, firebaseConfig };
+export { app, auth, db, rtdb, firebaseConfig };
